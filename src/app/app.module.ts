dee5572ef92ee5ecc7a853fce79e87c9dbbbb551
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-04-29 13:48:20 +0200

    taskbar and other componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskbarComponent } from './taskbar/taskbar.component';
-
+import { AddPersonComponent } from './add-person/add-person.component';
+import { ShowAllComponent } from './show-all/show-all.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TaskbarComponent
+    TaskbarComponent,
+    AddPersonComponent,
+    ShowAllComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
