c8bf6c7957efba5e88756ec358159b95c2b6c75b
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2024-07-18 20:34:23 +0200

    first few changes

--- src/app/high-light.directive.ts
+++ src/app/high-light.directive.ts
@@ -1,16 +1,31 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import {
+  Directive,
+  Renderer2,
+  OnInit,
+  ElementRef,
+  HostListener,
+  HostBinding,
+  Input,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighLight]',
 })
 export class HighLightDirective implements OnInit {
+  @Input('appHighLight') highLightColor = 'yellow';
+  @HostBinding('style.backgroundColor') backgroundColor: string = '';
+
   constructor(private ref: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    this.renderer.setStyle(
-      this.ref.nativeElement,
-      'background-color',
-      'yellow'
-    );
+    this.backgroundColor = this.highLightColor;
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.backgroundColor = 'transparent';
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.backgroundColor = this.highLightColor;
   }
 }
